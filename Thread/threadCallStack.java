0ee270755fc09572a08f0606786ebad27b54ffaf
Moon He Jow <gmlwh124@naver.com>
2022-12-05 18:44:37 +0900

    Thread Priority, call stack Practice

---
+++ Thread/threadCallStack.java
@@ -0,0 +1,23 @@
+package Thread;
+
+public class threadCallStack {
+    public static void main(String[] ars) {
+        Thread thread = new ThreadOne();
+        thread.start();
+    }
+}
+
+class ThreadOne extends Thread {
+    @Override
+    public void run() {
+        throwException();
+    }
+
+    public void throwException() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Thread/threadPriority.java
@@ -0,0 +1,12 @@
+package Thread;
+
+public class threadPriority {
+    public static void main(String[] args) {
+        Thread thread1 = new newThread();
+        Thread thread2 = new newThread();
+        thread2.setPriority(7);
+
+        System.out.println("상속받은 쓰레드 우선순위 : " + thread1.getPriority());
+        System.out.println("설정한 쓰레드 우선순위 : " + thread2.getPriority());
+    }
+}
