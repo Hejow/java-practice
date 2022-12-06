766e34faa4cb6efd62f6caef37b1b369297b1517
Moon He Jow <gmlwh124@naver.com>
2022-12-06 12:47:00 +0900

    Item1 Training

---
+++ EffectiveJava/Item1/StaticFactoryMethod.java
@@ -0,0 +1,55 @@
+package EffectiveJava.Item1;
+
+// public 생성자
+//class Pokemon {
+//    private int attack, defense, hp;
+//
+//    public Pokemon(int attack, int defense, int hp) {
+//        this.attack = attack;
+//        this.defense = defense;
+//        this.hp = hp;
+//    }
+//}
+//
+//public class StaticFactoryMethod {
+//    public static void main(String[] args) {
+//        Pokemon 피카츄 = new Pokemon(5,5,5);
+//    }
+//}
+
+// 정적 팩토리 메서드
+class Pokemon {
+    private int attack, defense, hp;
+
+    private Pokemon(int attack, int defense, int hp) {
+        this.attack = attack;
+        this.defense = defense;
+        this.hp = hp;
+    }
+
+    public static Pokemon newPokemon() {
+        return new Pokemon(10, 10, 10);
+    }
+}
+
+class Grade {
+     public static Grade of(int score) {
+         if (score == 100) {
+             return new Perfect();
+         } else if (score > 80) {
+             return new Good();
+         } else {
+             return new Soso();
+         }
+     }
+}
+
+class Perfect extends Grade {};
+class Good extends Grade {};
+class Soso extends Grade {};
+
+public class StaticFactoryMethod {
+    public static void main(String[] args) {
+        Pokemon 피카츄 = Pokemon.newPokemon();
+    }
+}
