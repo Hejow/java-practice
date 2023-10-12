bdb0179cbbbda3dd45abd78c413d4d03e6bfd6bd
Hejow <gmlwh124@naver.com>
2023-10-12 23:45:46 +0900

    async practice

---
+++ src/ETC/async/AsyncFactory.java
@@ -0,0 +1,24 @@
+package ETC.async;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncFactory {
+    private AsyncFactory() {
+    }
+
+    public static void runAsync() {
+        CompletableFuture.runAsync(() ->
+                System.out.println("Thread : " + Thread.currentThread().getName())
+        );
+
+        System.out.println("Thread : " + Thread.currentThread().getName());
+    }
+
+    public static void supplyAsync() {
+        CompletableFuture.supplyAsync(() ->
+                "Thread : " + Thread.currentThread().getName()
+        ).thenAccept(System.out::println);
+
+        System.out.println("Thread : " + Thread.currentThread().getName());
+    }
+}
---
+++ src/ETC/async/AsyncTest.java
@@ -0,0 +1,10 @@
+package ETC.async;
+
+public class AsyncTest {
+    public static void main(String[] args) {
+//        AsyncFactory.runAsync();
+
+        AsyncFactory.supplyAsync();
+
+    }
+}
